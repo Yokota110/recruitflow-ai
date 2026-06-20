9f099854b76dff3bdf2cf0dc99ebaa9175f64d57
Yokota Ishun <richunyokota93@gmail.com>
2026-06-21 01:35:13 +0900

    Fix Vercel Git deployment config

--- api/index.ts
+++ api/index.ts
@@ -4,11 +4,12 @@ let expressApp: ((req: unknown, res: unknown) => void) | undefined;
 
 export default async function handler(req: unknown, res: unknown) {
   if (!expressApp) {
-    const { createNestApp } = require('../dist/bootstrap') as {
+    const { createNestApp } = require('../apps/api/dist/bootstrap') as {
       createNestApp: () => Promise<{ expressApp: (req: unknown, res: unknown) => void }>;
     };
     const result = await createNestApp();
     expressApp = result.expressApp;
   }
+
   return expressApp(req, res);
 }
